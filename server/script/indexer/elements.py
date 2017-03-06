5b222e16a3f023e72bcffbbac9f82bc37684a2fb
zangshayang1 <zangshayang1@hotmail.com>
2017-03-06 14:12:21 -0800

    indexer demo suceed but better documentation needs to be done soon

--- server/script/indexer/elements.py
+++ server/script/indexer/elements.py
@@ -3,14 +3,13 @@ This module defines the element classes used in SearchEngine project
 '''
 
 import pickle
+import math
 
 class Term(object):
-    def __init__(self, term, tf, init_position):
-        self.term = term
+    def __init__(self, token, tf, init_position):
+        self.token = token
         self.tf = tf
         self.positions = [init_position]
-
-        self.idf = None # later computed
 
     def increment_tf(self):
         self.tf += 1
@@ -25,7 +24,26 @@ class Document(object):
         self.idx = idx
         self.filepath = filepath
         self.url = url
-        self.tf = None
+
+class InvDocument(Document):
+        ''' in inverted indexing table: each doc also has the following attrs so info about the indexing term can be easily stored/accessed.
+            these are set during _invert() process when each Document() obj meets Term() obj. '''
+        def __init__(self, idx, filepath, url, token, token_positions, token_tf):
+            ''' this is the best part No.1 of this project:
+            initialize what is defined in InvDocument's super class for the current class InvDocument. '''
+            super(self.__class__, self).__init__(idx, filepath, url)
+            self.token = token
+            self.token_positions = token_positions
+            self.token_tf = token_tf
+            self.token_idf = None
+
+        def set_idf(self, token_df, tot_docs):
+            '''
+            Given token's document frequency and the number of total documents.
+            This function computes the inverted document frequency.
+            '''
+            self.token_idf = math.log10(float(tot_docs) / token_df)
+            return ;
 
 
 
@@ -36,58 +54,58 @@ class IdxItemMap(object):
     """
     this is to facilitate indexing document process {idx: Document}
     """
-    def __init__(self, start_count = 0):
+    def __init__(self, start_idx = 0):
         self._map = {}
-        self._nextCount = start_count
+        self._idx = start_idx
 
-    def add_new(self, item):
-        self._map[self._nextCount] = item
-        self._nextCount += 1
+    def add(self, item, idx):
+        self._map[idx] = item
         return ;
 
-    def get_map(self):
-        return self._map
+    def get_item_at(self, idx):
+        return self._map[idx]
 
-    def get_next_count(self):
-        return self._nextCount
+    def next_avai_idx(self):
+        idx = self._idx
+        self._idx += 1
+        return idx
 
-class tknItemMap(object):
-    """
-    this is to facilitate mapping token process {token: Term}
-    """
-    def __init__(self):
-        self._map = {}
 
-    def add_new(self, token, item):
-        self._map[token] = item
-        return ;
-
-    def has(self, token):
-        return token in self._map
-
-    def remove(self, token):
-        print "Under dev..."
-        pass
-
-    def get_map(self):
-        return self._map
+# class tknItemMap(object):
+#     """
+#     this is to facilitate mapping token process {token: Term}
+#     """
+#     def __init__(self):
+#         self._map = {}
+#
+#     def add_new(self, token, item):
+#         self._map[token] = item
+#         return ;
+#
+#     def has(self, token):
+#         return token in self._map
+#
+#     def remove(self, token):
+#         print "Under dev..."
+#         pass
+#
+#     def get_map(self):
+#         return self._map
 
 
 class MapCacher(object):
     def __init__(self):
         self.docMap = {}
-        self.termMap = {}
 
-    def init_docMap(self, name, doc_start_idx = 0):
+    def init_docMap(self, name = 'main', doc_start_idx = 0):
         '''
         after calling this function, a document mapping dictionary and initial doc index are created and associated with the given name within self.docMap
         '''
         self.docMap[name] = IdxItemMap(doc_start_idx)
         return ;
 
-    def init_termMap(self, name):
-        self.termMap[name] = tknItemMap()
-        return ;
+    def get_docMap(self, name = 'main'):
+        return self.docMap[name]
 
     def load(self, filepath, mode = 'pickle'):
         if mode == 'pickle':
@@ -98,10 +116,10 @@ class MapCacher(object):
         else:
             raise Exception('Error: "mode" can be either "pickle" or "json".')
 
-    def store(self, mapdict, filepath, mode = 'pickle'):
+    def save(self, filepath, mode = 'pickle'):
         if mode == 'pickle':
             with open(filepath, 'wb') as f:
-                pickle.dump(mapdict, f)
+                pickle.dump(self.docMap, f)
                 return ;
         elif mode == 'json':
             raise Exception('Under dev...')
--- server/script/indexer/indexer.py
+++ server/script/indexer/indexer.py
@@ -1,125 +1,207 @@
-from elements import IndexStructure
-from DocAnalyzer import DocAnalyzer
-
-
-from element import Document, MapCacher
-import os
+from docAnalyzer import DocAnalyzer
+from nltk.tokenize import RegexpTokenizer
+from elements import Document, MapCacher, InvDocument
 import json
+import pickle
+import os
+import time
 
 class Indexer(object):
 
-	"""
-	If webpages come in in batches. Given the root dir of this batch of data, Indexer() take care of it.
-	"""
-	def __init__(self, dir):
-		self.dir = dir
-		self.bk = None
-		self.mainIndex = {}
 
-	def load_booKeeping_from(filepath, mode = 'json'):
+	def __init__(self, bkpFile):
+		''' load bookkeeping file no matter it is in building or loading mode '''
+		self.bkp = self._load_booKeeping_from(bkpFile)
+
+
+	''' Everything initialized by this method is global to this class so self.mainProcess() can use them. '''
+	def _init_for_building(self, kwargs):
+		''' total doc count '''
+		self.tot_docs = 0
+
+		''' init empty document indexing cache mapper, only one docMap needed with default name is 'main '''
+		self.mc = self._init_mapCacher()
+		self.mainDocMap = self.mc.get_docMap()
+
+		''' this is the best part of the project No.2: the use of kwargs is maximized this way. '''
+		self.index = self._init_index(kwargs)
+		self.invIndex = self._init_invIdex(kwargs)
+		self.docAnalyzer = DocAnalyzer(RegexpTokenizer(r'\w+'))
+
+		return ;
+
+
+	def _init_index(self, params):
+		index = {}
+		for k in params:
+			index[k] = {}
+		return index
+
+	def _init_invIdex(self, params):
+		invIndex = {}
+		for k in params:
+			invIndex[k] = None
+		return invIndex
+
+	def _load_booKeeping_from(self, bkpFile, mode = 'json'):
 		if mode == 'json':
-			with open(filepath, 'r') as handler:
-				self.bk = json.load(handler)
-				return ;
+			with open(bkpFile, 'r') as handler:
+				return json.load(handler)
 		else:
 			raise Exception("Error in Indexer: other mode under dev...")
 
 	def _init_mapCacher(self):
 		mc = MapCacher()
-		mc.init_docMap('main')
-		mc.init_termMap('title')
-		mc.init_termMap('header')
-		mc.init_termMap('body')
+		mc.init_docMap()
 		return mc
 
-	def mainProcess(self):
-		mc = self._init_mapCacher():
+	def mainProcess(self, build_invIndex_of = None, save_docIndex_at = None, save_invIndex_at = None, **kwargs):
+		if build_invIndex_of is None or save_docIndex_at is None or save_invIndex_at is None:
+			raise Exception('Indexer.mainProcess(): Missing arguments.')
 
-		for root, dirs, files in os.walk(self.dir):
+		print 'Start to build...'
+
+		''' init self.tot_docs, self.mc, self.mainDocMap, self.index, self.invIndex, self.docAnalyzer '''
+		self._init_for_building(kwargs)
+
+		''' the following traversing strategy looks ugly. a separate method needed to scale the app. '''
+		for root, dirs, files in os.walk(build_invIndex_of):
 			for f in files:
+				self.tot_docs += 1
 				filepath = os.path.join(root, f)
-				relapath = os.path.relpath(filepath, self.dir)
-				url = self.bk[relapath]
+				relapath = os.path.relpath(filepath, build_invIndex_of)
 
-				mainDoc = mc.docMap['main']
-				docIdx = mainDoc.get_next_count() # for future reference via docNum int
-				doc = Document(docIdx, filepath, url) # create new Document obj
-				mainDoc.add_new(doc) # form mapping relation
+				'''  mac sys checkpoint '''
+				if relapath == '.DS_Store':
+					continue
 
-				tk_dict = newDoc.tokenize() # tokenization
+				url = self.bkp[relapath] # find its corresponding url in bookkeeping
+				docIdx = self.mainDocMap.next_avai_idx() # retrieve the next avaliable spot in the docMap
+				doc = Document(docIdx, filepath, url) # create new Document obj with the retrieved idx
+				self.mainDocMap.add(doc, docIdx) # form mapping relation
 
-				"""
-				when query comes in as a stream of strings, 3 indexing dictionaries
-				with term strings as keys are needed for quick reference. Thus, 3 mapping relations are needed in MapCaher. Name each sub dictionary a name.
+				stats = self.docAnalyzer.analyze(filepath)
 
-				"""
+				for k in kwargs:
+					if kwargs[k]:
+						self.index[k][docIdx] = stats[k]
+					# if kwargs[k] is False:
+					# 	self.index[k] = {} as it is initialized
 
-	def invert(self):
-		pass
+		# self.index is an intermediate result, no need to save or restore.
+		print 'Doc-Term Indexing done!'
+		self.save(self.mainDocMap, save_docIndex_at, mode = 'pickle')
+		print 'Doc-Idx Mapping Done!'
 
+		for k in kwargs:
+			if kwargs[k]:
+				if k == 'anchor':
+					self.invIndex[k] = self._invert(self.index[k], anchor = True)
+				else:
+					self.invIndex[k] = self._invert(self.index[k], anchor = False)
 
-
-
-
-
-
-
-
-
-
-
-
-class yIndexer():
-
-	def __init__(self):
-		self.analyer = DocAnalyzer()
-
-	def processing(self, directory):
-		with open('test.json', "r") as f:
-			data = json.load(f)
-			for root, dirs, files in os.walk(directory):
-				for file in files:
-					path = os.path.join(root, file)
-					rel_path = os.path.relpath(path, directory)
-					print rel_path
-					title, content = self.analyer.extractData(path)
-					tokens = self.analyer.tokenize(content)
-					title_tokens = self.analyer.tokenize(title)
-					token_position = self.analyer.token_with_position(tokens)
-					title_token_position = self.analyer.token_with_position(title_tokens)
-					token_freq = self.analyer.frequency(token_position)
-					title_token_freq = self.analyer.frequency(title_token_position)
-					token_tf = self.analyer.tf(token_freq)
+		self.save(self.invIndex, save_invIndex_at)
+		print 'Token-Doc Inverted Indexing Done!'
+		return ;
 
 
-					for token in token_freq.keys():
-						if data.has_key(token):
-							data[token]['df'] += 1
-							data[token]['idf'] = self.analyer.idf(data[token]['df'])
-							if token in title_tokens:
-								data[token]['docs'][rel_path] = {"tf": token_tf[token], "pos":token_position[token], "title_pos": title_token_position[token]}
-							else:
-								data[token]['docs'][rel_path] = {"tf": token_tf[token], "pos":token_position[token], "title_pos": []}
+	def _invert(self, index, anchor):
+		'''
+		This function involves a few variables global to this class. It must be called during self.mainProcess().
+		--------------------------------------------------------------------------------------------------------
+		If anchor is False:
+			Input:
+				{docIdx<Int>: {url<String>: [token<String>]}}
+			Output:
+				{token<String>: [url1, url2, ..., url3]}
+
+		if anchor is True:
+			Input:
+				{docIdx<Int>: {token<String>: term<Term>}} # where no duplicated tokens present
+			Output:
+				{token<String>: [Document1, Document2, ..., DocumentN]}
+		'''
+		inverted = {}
+
+		if anchor:
+			for docIdx, url_tokens in index.iteritems():
+				for url, tokens in url_tokens.iteritems():
+					for token in tokens:
+						if token in inverted:
+							inverted[token].add(url)
 						else:
-							df = 1
-							idf = self.analyer.idf(df)
-							filename = rel_path
-							pos = token_position[token]
-							tf = token_tf[token]
-							if token in title_tokens:
-								title_pos = title_token_position[token]
-							else:
-								title_pos = []
-							args = [df, idf, filename, tf, pos, title_pos]
-							index = IndexStructure(args)
-							structure = index.structure
-							data[token] = structure
+							''' ranking strategy: query comes in; relevant docs found; relevant urls found; intersect. '''
+							inverted[token] = set([url]) # make sure there is no duplicated urls and provide O(1) lookup efficiency during ranking.
 
-		with open('test.json', 'w') as f:
-			json.dump(data, f, indent=2)
+			return inverted
+
+		for docIdx, tkMap in index.iteritems():
+			doc = self.mainDocMap.get_item_at(docIdx) # all the tokens in mainDocMap[docIdx]
+			for token in tkMap:
+				term = tkMap[token]
+				inv_doc = InvDocument(doc.idx, doc.filepath, doc.url, term.token, term.positions, term.tf)
+
+				if token in inverted:
+					inverted[token].append(inv_doc) # no duplicated tokens present in the same document but they do occur over a bunch of other documents
+				else:
+					inverted[token] = [inv_doc]
+
+		for token in inverted:
+			df = len(inverted[token])
+			for inv_doc in inverted[token]:
+				inv_doc.set_idf(df, self.tot_docs)
+
+		return inverted
 
 
-directory = '/Users/shayangzang/Desktop/cs211-infoRetrieval/project3/WEBPAGES_RAW'
+	'''
+	Indexer.restore() help cache the inverted index result file into memory for ranking use.
+	'''
+	def restore(self, load_docIndex_from = None, load_invIndex_from = None):
+		if load_docIndex_from is None or load_invIndex_from is None:
+			raise Exception('Indexer.restore(): Missing argument.')
+		self.mainDocMap = self.load(load_docIndex_from, mode = 'pickle')
+		self.invIndex = self.load(load_invIndex_from, mode = 'pickle')
+		print 'Data Restore Done!'
+		return ;
 
-testing = Indexer()
-testing.processing(directory)
+
+	def load(self, filepath, mode = 'pickle'):
+		if mode == 'pickle':
+			with open(filepath, 'rb') as f:
+				return pickle.load(f)
+		elif mode == 'json':
+			raise Exception('Under dev...')
+		else:
+			raise Exception('Error: "mode" can be either "pickle" or "json".')
+
+
+	def save(self, data, filepath, mode = 'pickle'):
+		if mode == 'pickle':
+			with open(filepath, 'wb') as f:
+				pickle.dump(data, f)
+				return ;
+		elif mode == 'json':
+			raise Exception('Under dev...')
+		else:
+			raise Exception('Error: "mode" can be either "pickle" or "json".')
+
+
+
+
+t = time.time()
+workingDir = '/Users/shayangzang/Desktop/cs211-infoRetrieval/project3/testWebpage/'
+bkpFile = '/Users/shayangzang/Desktop/cs211-infoRetrieval/project3/metadata/bookkeeping.json'
+docIndexFile = '/Users/shayangzang/Desktop/cs211-infoRetrieval/project3/metadata/docIndex.pickle'
+invIndex = '/Users/shayangzang/Desktop/cs211-infoRetrieval/project3/metadata/invIndex.pickle'
+
+indexer = Indexer(bkpFile)
+indexer.mainProcess(build_invIndex_of = workingDir, save_docIndex_at = docIndexFile, save_invIndex_at = invIndex, title = True, header = True, body = True, anchor = True)
+print "invert indexing takes", time.time() - t
+t = time.time()
+
+indexer.restore(load_docIndex_from = docIndexFile, load_invIndex_from = invIndex)
+print "restore indexing takes", time.time() - t
+
+iiMap = indexer.invIndex
+print iiMap['body']['machine']
